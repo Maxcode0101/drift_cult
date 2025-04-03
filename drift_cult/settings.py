b0e471fc999343370d186466b86684621acc9b79
MaxCode0101 <maximilian.kaening@gmail.com>
2025-04-03 14:58:27 +0200

    Set up product detail functionality, media config, and template structure.

--- drift_cult/settings.py
+++ drift_cult/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'drift_cult.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -146,6 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
