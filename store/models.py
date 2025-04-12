ceb629127e0e32ad287a415ba4379d2e90a64bc6
MaxCode0101 <maximilian.kaening@gmail.com>
2025-04-12 19:55:06 +0200

    Add ProductSize model with CartItem and OrderItem relations

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -24,3 +26,36 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ProductSize(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='sizes')
+    size = models.CharField(max_length=10)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.size}'
+    
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_size = models.ForeignKey('ProductSize', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product_size}'
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Order #{self.id} by {self.user.username}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product_size = models.ForeignKey('ProductSize', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product_size}'
