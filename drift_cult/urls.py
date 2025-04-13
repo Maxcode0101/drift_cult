2884126819ee601df07e06faec27c4984254b983
MaxCode0101 <maximilian.kaening@gmail.com>
2025-04-13 19:50:34 +0200

    Clean urls.py - favicon handled via static in base template

--- drift_cult/urls.py
+++ drift_cult/urls.py
@@ -1,23 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home
-from core import views as core_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
-
+from core.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', home, name='home'),
-    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'), permanent=True)),
     path('product/', include('store.urls')),
     path('store/', include('store.urls')),
-    path('', include('core.urls')),
-    path('', core_views.home, name='home'),
+    path('', home, name='home'),
 ]
 
-# Serve media files during development
+# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/urls.py
+++ store/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 from .views import ProductListView
 
-
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('', views.shop_view, name='shop'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('profile/', views.profile_view, name='profile'),
     path('cart/', views.view_cart, name='view_cart'),
