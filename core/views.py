b780478d66a660e16b47e0e1d04de3dc79f2886d
MaxCode0101 <maximilian.kaening@gmail.com>
2025-04-16 12:05:51 +0200

    Add home view to render core/home.html

--- core/views.py
+++ core/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 def home(request):
-    return HttpResponse("Welcome to Drift Cult!")
+    return render(request, 'core/home.html')
