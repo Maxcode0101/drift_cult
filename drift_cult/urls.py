9ce348127349020c39ee5181a90db1925a15c6f5
MaxCode0101 <maximilian.kaening@gmail.com>
2025-04-23 10:45:39 +0200

    Fix: Add webhook import to urls.py

--- drift_cult/urls.py
+++ drift_cult/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import home
+from store.views import stripe_webhook
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
