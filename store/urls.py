c9028d12fb0e34f5b1e75dec7bc8f8b20ee9e807
MaxCode0101 <maximilian.kaening@gmail.com>
2025-04-24 10:03:56 +0200

    Add URL for update_cart_quantity view to support cart quantity updates

--- store/urls.py
+++ store/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('cart/', views.view_cart, name='view_cart'),
     path('add-to-cart/<int:product_id>/<int:size_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('update_cart_quantity/<int:item_id>/', views.update_cart_quantity, name='update_cart_quantity'),
 
     # Stripe Checkout
     path('checkout/', views.checkout, name='checkout'),
