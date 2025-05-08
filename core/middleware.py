0d1bd86bb3f9bcf413710bb11bc0af5181ca75e0
MaxCode0101 <maximilian.kaening@gmail.com>
2025-05-08 08:08:33 +0200

    Feat: enforce canonical domain driftcult.art with middleware redirect

---
+++ core/middleware.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponsePermanentRedirect
+
+
+class CanonicalDomainRedirectMiddleware:
+    """
+    Redirects all traffic from www.driftcult.art to driftcult.art
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        host = request.get_host()
+        if host.startswith('www.'):
+            new_url = request.build_absolute_uri().replace('://www.', '://', 1)
+            return HttpResponsePermanentRedirect(new_url)
+        return self.get_response(request)
--- drift_cult/settings.py
+++ drift_cult/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'core.middleware.CanonicalDomainRedirectMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
