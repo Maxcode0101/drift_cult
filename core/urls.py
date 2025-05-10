810d537a4033269b2d2e26a0f882e9cbf3b17574
MaxCode0101 <maximilian.kaening@gmail.com>
2025-05-10 05:49:12 +0200

    Add sitemap.xml route to core urls

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
+from django.contrib.sitemaps.views import sitemap
 from .views import stripe_webhook, test_email, robots_txt
 from store.views import home_view, about_view, community_view
+from store.sitemaps import ProductSitemap, StaticViewSitemap
+
+sitemaps = {
+    'products': ProductSitemap(),
+    'static': StaticViewSitemap(),
+}
 
 urlpatterns = [
     path("", home_view, name="home"),
-    path('webhook/stripe/', stripe_webhook, name='stripe_webhook'),
+    path("webhook/stripe/", stripe_webhook, name="stripe_webhook"),
     path("test-email/", test_email, name="test_email"),
     path("about/", about_view, name="about"),
     path("community/", community_view, name="community"),
     path("robots.txt", robots_txt, name="robots_txt"),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
