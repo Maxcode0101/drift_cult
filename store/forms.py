a273cbbe4c8822fa6e7e9d479ec478b6314ed2a9
MaxCode0101 <maximilian.kaening@gmail.com>
2025-05-10 09:29:06 +0200

    Add view and template for adding/editing product sizes via admin dashboard

--- store/forms.py
+++ store/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Product
+from .models import Product, ProductSize
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'category', 'image']
+        fields = ['name', 'description', 'price', 'category', 'image',]
